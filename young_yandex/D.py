24f6e87322abea6da890c61e9241cd7f92a88906
bon <bonujumanova@gmail.com>
2025-10-03 20:43:33 +0700

    ошибка PE

--- young_yandex/D.py
+++ young_yandex/D.py
@@ -7,12 +7,11 @@ def main():
 
     count_exercises_by_themes: dict[int, int] = {}
     for item in themes:
-        if item in count_exercises_by_themes:
-            count_exercises_by_themes[item] += 1
-        else:
-            count_exercises_by_themes[item] = 0
+        count_exercises_by_themes[item] = count_exercises_by_themes.get(item, -1) + 1
+
     # exercises: list = sorted(list(count_exercises_by_themes.keys()))
     exercises: list[int] = list(set(themes))
+
 
     for exercise, total_exercis_by_one_theme in count_exercises_by_themes.items():
         if len(exercises) != k:
