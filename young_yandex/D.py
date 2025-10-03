63709a4b3d9ab4d25fa090b9a0a53ffa5d928c75
bon <bonujumanova@gmail.com>
2025-10-03 20:34:46 +0700

    ошибка PE

--- young_yandex/D.py
+++ young_yandex/D.py
@@ -3,36 +3,25 @@ import sys
 
 def main():
     n, k = input().split()
-    themes = map(str, input().split())
+    themes = list(map(int, input().split()))
 
-    count_exercises_by_themes = {}
+    count_exercises_by_themes: dict[int, int] = {}
     for item in themes:
         if item in count_exercises_by_themes:
             count_exercises_by_themes[item] += 1
         else:
             count_exercises_by_themes[item] = 0
-    exercises: list = sorted(list(count_exercises_by_themes.keys()))
+    # exercises: list = sorted(list(count_exercises_by_themes.keys()))
+    exercises: list[int] = list(set(themes))
 
     for exercise, total_exercis_by_one_theme in count_exercises_by_themes.items():
         if len(exercises) != k:
             if total_exercis_by_one_theme > 0:
                 exercises.append(exercise)
                 count_exercises_by_themes[exercise] -= 1
-        print(' '.join(exercises))
+        print(*exercises)
         break
 
 
 if __name__ == '__main__':
     main()
-
-
-
-# exercises = list(set(themes))
-# if len(exercises) < k:
-#     while len(exercises) != k:
-#         for char in
-#
-#         exercises.append()
-#
-#
-
